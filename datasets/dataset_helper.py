67afdcd529199aa78135eb340e0731e0510e29bb
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-05-29 18:06:16 +0530

    Fixed us dataset

--- datasets/dataset_helper.py
+++ datasets/dataset_helper.py
@@ -1,13 +1,26 @@
 import pandas as pd
 
-# Load the dataset
-df = pd.read_csv('indian_attractions_final.csv')
+# # Load the dataset
+# df = pd.read_csv('indian_attractions_final.csv')
 
-# Drop the 'Country' column
-df = df.drop('Country', axis=1)
+# # Drop the 'Country' column
+# df = df.drop('Country', axis=1)
+
+# # Save the updated dataframe back to the same CSV file
+# df.to_csv('indian_attractions_final.csv', index=False)
+
+# # Print the updated dataframe
+# print(df)
+
+
+# Load the dataset with error handling
+df = pd.read_csv('final-us.csv', on_bad_lines='skip')
+
+# Remove duplicates based on the 'Name' and 'City' columns
+df = df.drop_duplicates(subset=['Name', 'City'])
 
 # Save the updated dataframe back to the same CSV file
-df.to_csv('indian_attractions_final.csv', index=False)
+df.to_csv('final-us.csv', index=False)
 
 # Print the updated dataframe
 print(df)
