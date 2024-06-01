a8e1bbdd2a28da897f83c783dcf015bf8061362f
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-06-01 14:30:41 +0530

    Organized helper files

--- deploy/recommendations_streamlit.py
+++ deploy/recommendations_streamlit.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 def load_data():
-    attractions_data = pd.read_csv('datasets/final_attractions.csv', usecols=['Name', 'State', 'City', 'Opening Hours', 'Description'])
-    user_ratings_data = pd.read_csv('datasets/user_ratings.csv', usecols=['Attraction', 'Noel', 'Harsh', 'Vikranth', 'Muthuraj', 'Armaan'])
+    attractions_data = pd.read_csv('../datasets/final_attractions.csv', usecols=['Name', 'State', 'City', 'Opening Hours', 'Description'])
+    user_ratings_data = pd.read_csv('../datasets/user_ratings.csv', usecols=['Attraction', 'Noel', 'Harsh', 'Vikranth', 'Muthuraj', 'Armaan'])
     return attractions_data, user_ratings_data
 
 
 def load_user_data(user):
-    attractions_data = pd.read_csv('datasets/final_attractions.csv', usecols=['Rating', 'Name', 'State', 'City', 'Country', 'Opening Hours', 'Description'])
-    user_ratings_data = pd.read_csv('datasets/user_ratings.csv', usecols=['Attraction', user])
+    attractions_data = pd.read_csv('../datasets/final_attractions.csv', usecols=['Rating', 'Name', 'State', 'City', 'Country', 'Opening Hours', 'Description'])
+    user_ratings_data = pd.read_csv('../datasets/user_ratings.csv', usecols=['Attraction', user])
     
     attractions_data.rename(columns={'Rating': 'Google_Rating'}, inplace=True)
     attractions_data['User_Rating'] = user_ratings_data[user]
