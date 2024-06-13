adfc3079612720e3b0cb536f688819bccf918c64
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-06-13 23:33:41 +0530

    Updated entity extraction and added summarization

--- NLP-Processing/spacy_processing.py
+++ NLP-Processing/spacy_processing.py
@@ -1,47 +1,52 @@
-from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.corpus import stopwords
 import spacy
 import nltk
+import re
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
 
 nltk.download('punkt')
 nltk.download('stopwords')
 
-stopwords = set(stopwords.words('english'))
+stopwords_set = set(stopwords.words('english'))
 nlp_model = spacy.load('en_core_web_sm')
-data = open("../webscraped data/traveltriangle.txt", "r").read()
-data_processed = nlp_model(data)
+
+with open("../webscraped data/traveltriangle.txt", "r") as file:
+    data = file.read()
 
 sentences = sent_tokenize(data)
 attractions = {}
 current_index = 0
-for index in range(len(sentences)):
-    sentence = sentences[index]
+
+for sentence in sentences:
     words = word_tokenize(sentence)
-    words = [word for word in words if word.isalnum() and word not in stopwords]
-    sentences[index] = " ".join(words)
+    words = [word for word in words if word.isalnum() and word.lower() not in stopwords_set]
+    cleaned_sentence = " ".join(words)
 
-    
-    if sentences[index].isdigit():
-        current_index = int(sentences[index])
+    match = re.match(r'^(\d+)\.\s*', sentence)
+    if match:
+        current_index = int(match.group(1))
         attractions[current_index] = []
-    
-    else:
-        if current_index:
-            attractions[current_index].append(sentences[index])
+    elif current_index:
+        attractions[current_index].append(cleaned_sentence)
 
+entities = {}
+for idx in attractions:
+    combined_text = " ".join(attractions[idx])
+    doc = nlp_model(combined_text)
+    entities[idx] = [ent.text for ent in doc.ents if ent.label_ in ['GPE', 'LOC', 'ORG', 'PERSON']]
 
+from gensim.summarization import summarize
 
-result = {}
+summarized_attractions = {}
+for idx, texts in attractions.items():
+    full_text = " ".join(texts)
+    try:
+        summary = summarize(full_text)
+    except ValueError:
+        summary = full_text
+    summarized_attractions[idx] = summary
 
-write_file = open("../after_scraping/traveltriangle_after.txt", "w")
-for i in attractions:
-    result[attractions[i][0]] = ""
-    for j in range(1, len(attractions[i])):
-        result[attractions[i][0]] += attractions[i][j] + ". "
-    
-
-    write_file.write(attractions[i][0] + "\n")
-    write_file.write(result[attractions[i][0]] + "\n\n")
-
-write_file.close()
-
+with open("../after_scraping/traveltriangle_after.txt", "w") as write_file:
+    for idx in summarized_attractions:
+        write_file.write(f"{idx}. {entities[idx][0] if entities[idx] else 'Unknown Attraction'}\n")
+        write_file.write(f"{summarized_attractions[idx]}\n\n")
