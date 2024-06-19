fa898a6797b0adab379680d4e14bc9e0fadd728c
noeltiju <noelab04@gmail.com>
2024-06-20 00:27:06 +0530

    Waiting for Harsh to run llama text generation in order to test the current capabilities.

--- NLP Processing/scripts/lllama_pipeline.py
+++ NLP Processing/scripts/lllama_pipeline.py
@@ -1,7 +1,14 @@
 from transformers import pipeline
+import torch
+import json
 
 pipe = pipeline("text-generation", model="meta-llama/Meta-Llama-3-8B-Instruct")
-
+# if torch.backends.mps.is_available():
+#     device = torch.device("mps")
+#     print("MPS is available. Using MPS device.")
+# else:
+#     device = torch.device("cpu")
+#     print("MPS device not found. Using CPU.")
 def return_output(input):
 
     input = "Summarize: " + input
@@ -11,3 +18,26 @@ def return_output(input):
 
     response = pipe(messages)
     return response[0]["generated_responses"]
+
+
+files = [
+    'fine-tuning-goa_traveltriangle.json',
+    'fine-tuning-italy_traveltriangle.json',
+    'fine-tuning-japan_traveltriangle.json',
+    'fine-tuning-kerala_traveltriangle.json',
+    'fine-tuning-lakshwadeep_traveltriangle.json',
+    'fine-tuning-makemytrip.json',
+    'fine-tuning-tn_traveltriangle.json',
+    'fine-tuning-vietnam_traveltriangle.json'
+]
+
+for i in files:
+    data = json.load(open(f"../after_scraping/Context-Data/{i}", "r"))
+    write_file =  open(f"../after_scraping/Summarized-LLAMA/summarized-{i}", "w")
+    json_data = {}
+    for id, context in data.items():
+        d = {"context": context, "summary": return_output(context)}
+        json_data[id] = d
+
+    json.dump(json_data, write_file, indent=4)
+    
