d95c38d5eddd21e4174a9e834ee247940a9bee72
noeltiju <noelab04@gmail.com>
2024-06-24 09:55:27 +0530

    Pushed datasets in the Manual Summarized folder.

---
+++ NLP Processing/scripts/summarized_file_generator.py
@@ -0,0 +1,19 @@
+import os
+import json
+def return_output():
+    files = os.listdir("../after_scraping/Context-Data/Europe")
+    
+    for file in files:
+        data = json.load(open(f"../after_scraping/Context-Data/Europe/{file}", "r"))
+        write_file =  open(f"../after_scraping/Manual Summarized/Europe/{file}", "w")
+
+
+        json_data = {}
+        for id, context in data.items():
+            d = {"context": context, "summary": ""}
+            json_data[id] = d
+
+        json.dump(json_data, write_file, indent=4)
+
+    
+return_output()
